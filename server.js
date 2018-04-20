7cee8c150e56e82a8ac2501991486b44b37fbc61
Harold Davidson <harold.davidson89@gmail.com>
2018-04-19 19:52:05 -0500

    routes started

--- server.js
+++ server.js
@@ -14,6 +14,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
+var reservations = [
+  {
+    uniqueID: "harold",
+    name: "Harold",
+    phoneNumber: 8675309,
+    email: "dummy@example.com"
+  }
+  
+];
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/api/tables", function(req, res) {
+
+  
+  return res.json(reservations);
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
